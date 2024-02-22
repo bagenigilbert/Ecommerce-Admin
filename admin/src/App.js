a25d5f19b2f2c48f0ec066e65455ad96a15bf227
gilbert <bagenigilbert@gmail.com>
2024-02-23 02:26:41 +0300

    added ant-design-charts

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,15 +9,16 @@ import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/reset-password" element={<Resetpassword />} />
-          <Route path="/forgot-password" element={<Forgotpassword />} />
-          <Route path="/admin" element={<MainLayout />} /> 
-          {/* <Route index element={<Dashboard />} /> */}
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/reset-password" element={<Resetpassword />} />
+        <Route path="/forgot-password" element={<Forgotpassword />} />
+        <Route path="/admin" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
     </div>
   );
 }
--- admin/src/pages/Dashboard.js
+++ admin/src/pages/Dashboard.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { BsArrowDownRight, BsArrowUpRight } from "react-icons/bs";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+    <h3 className="mb-4 title">Dashboard</h3>
+      <div className="d-flex justify-content-between align-items-center gap-3">
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">
+          <div>
+            <p className="desc">Total</p>
+            <h4 className="mb-0 sub-title">$1500</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6>
+            <BsArrowDownRight /> 35%
+            </h6>
+            <p className="mb-0  desc">Compared To February 2024</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">
+          <div>
+            <p className="desc">Total</p>
+            <h4 className="mb-0 sub-title">$1500</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+            <BsArrowDownRight /> 35%
+            </h6>
+            <p className="mb-0  desc">Compared To February 2024</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 roudned-3">
+          <div>
+            <p className="desc">Total</p>
+            <h4 className="mb-0 sub-title">$1500</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+            <BsArrowDownRight />35%
+            </h6>
+            <p className="mb-0 desc">Compared To February 2024</p>
+          </div>
+        </div>
+      </div>
+      </div>
+   
+     )
 }
 
 export default Dashboard
